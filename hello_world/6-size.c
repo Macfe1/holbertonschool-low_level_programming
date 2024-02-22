ddb8976287b772e9181ad0657e601d5dc74934c2
Macfe1 <mac1515.pp@gmail.com>
2024-02-22 16:16:27 -0500

    Agruegué 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 
-printf("Size of a char: %zu\n", sizeof(char));
-printf("Size of an int: %zu\n", sizeof(int));
-printf("Size of a long int: %zu\n", sizeof(long int));
-printf("Size of a long long int: %zu\n", sizeof(long long int));
-printf("Size of a float: %zu\n", sizeof(float));
+printf("Size of a char: %zu\n byte(s)", sizeof(char));
+printf("Size of an int: %zu\n byte(s)", sizeof(int));
+printf("Size of a long int: %zu\n byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu\n byte(s)", sizeof(long long int));
+printf("Size of a float: %zu\n byte(s)", sizeof(float));
 
 return (0);
 
