f0cfc9ea3f9f4a907e5c8f74046625ac935215a9
Macfe1 <mac1515.pp@gmail.com>
2024-02-21 19:17:32 -0500

    Agregando 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - principla function
+ *
+ * Return: 0 if the function works
+ *
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
