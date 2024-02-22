0640a8bb464036979fcf313ce0c46491d2befb0a
Macfe1 <mac1515.pp@gmail.com>
2024-02-22 15:50:24 -0500

    Agruegué 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - principal function
+*
+* Return: 0 if the function is zero then success
+*/
+
+int main(void)
+{
+
+printf("Size of a char: %zu\n", sizeof(char));
+printf("Size of an int: %zu\n", sizeof(int));
+printf("Size of a long int: %zu\n", sizeof(long int));
+printf("Size of a long long int: %zu\n", sizeof(long long int));
+printf("Size of a float: %zu\n", sizeof(float));
+
+return (0);
+
+}
