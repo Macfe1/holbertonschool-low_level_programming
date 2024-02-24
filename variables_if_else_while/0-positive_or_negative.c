22e4896889f24abe371ce1de5dc17730f559b25e
Macfe1 <mac1515.pp@gmail.com>
2024-02-24 13:43:30 -0500

    Mejorando 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,13 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0 || n == 0)
+if (n > 0)
 {
 printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero", n);
 }
 else
 {
