a5d8901b10fbbae97aeb476c1ff7b19d5ba21ca4
Macfe1 <mac1515.pp@gmail.com>
2024-02-24 13:22:17 -0500

    Agregue 0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - principal function
+*
+* Return: 0 if the function is zero then success
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0 || n == 0)
+{
+printf("%d is positive\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
