d09833f457faef37967978f57161824e6764a72c
Macfe1 <mac1515.pp@gmail.com>
2024-02-24 13:49:49 -0500

    Agregando 0-positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is zero\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
