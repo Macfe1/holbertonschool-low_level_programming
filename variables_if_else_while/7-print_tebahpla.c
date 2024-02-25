2b4b4858f888ee25d11cef8c37df7893ee8108fb
Macfe1 <mac1515.pp@gmail.com>
2024-02-25 13:20:17 -0500

    Agregando 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Principal Function
+*
+*Return: 0 if that is the anwser the succes
+*/
+
+int main(void)
+{
+char letters;
+for (letters = 'z'; letters >= 'a'; letters--)
+{
+
+putchar(letters);
+
+}
+
+putchar('\n');
+return (0);
+
+}
