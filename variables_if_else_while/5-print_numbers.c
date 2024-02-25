1363df7bb3e9165c186950d7300257d377621766
Macfe1 <mac1515.pp@gmail.com>
2024-02-25 12:28:38 -0500

    Agregando 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - principal function
+*
+*Return: 0 if the 0 is teh answer success
+*/
+int main(void)
+{
+int num;
+for (num = 1; num <= 9; num++)
+{
+
+printf("%d", num);
+
+}
+
+printf("\n");
+return (0);
+}
