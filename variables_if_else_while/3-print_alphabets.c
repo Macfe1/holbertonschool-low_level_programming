6e961fdd9513c19373cde7bdb1ee3ffcbbd2b0be
Macfe1 <mac1515.pp@gmail.com>
2024-02-25 11:01:54 -0500

    Agregue 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - principal function
- *
- * Return: 0 if the program is successful
- */
+*main - principal function
+*
+*Return: 0 if the 0 is teh answer success
+*/
 
 int main(void)
 {
-char low = 'a';
-
-while (low <= 'z' || low <= 'Z')
+char letra;
+for (letra = 'a'; letra <= 'z'; letra++)
 {
-putchar(low);
-if (low == 'z')
-{
-low = 'A';
+putchar(letra);
+putchar(' ');
 }
-low++;
+
+for (letra = 'A'; letra <= 'Z'; letra++)
+{
+putchar(letra);
+if (letra != 'Z')
+{
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
