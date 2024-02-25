8570eb24ea5ccb9737f1adcbe33b1e6472c3f0ec
Macfe1 <mac1515.pp@gmail.com>
2024-02-25 12:48:59 -0500

    Agregando 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Principal Function
+*
+*Return: 0 if that is the anwser the succes
+*/
+
+int main(void)
+{
+int mero;
+for (mero = 0; mero <= 9; mero++)
+{
+putchar('0' + mero);
+}
+putchar('\n');
+return (0);
+}
