43202d1917058fd694afc166796cbaffea81212d
Macfe1 <mac1515.pp@gmail.com>
2024-02-28 12:01:51 -0500

    Agregando 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet - nn
+ *
+ *Return: nn
+ */
+
+void print_alphabet(void)
+{
+
+	char n;
+
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+
+		_putchar(n);
+	}
+
+
+	_putchar('\n');
+}
