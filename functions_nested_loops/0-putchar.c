812ca479054252ed365ff81f71ab60fd2697ae68
Macfe1 <mac1515.pp@gmail.com>
2024-04-16 21:53:22 -0500

    Agregando 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - principal function to print _putchar
+*
+* Return: 0 if the function is zero then success
+*/
+
+int main (void)
+{
+  putchar ('_putchar');
+  return (0);
+}	
