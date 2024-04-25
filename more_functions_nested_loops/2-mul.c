9158a776375ffdd5b98e0c6c10fefa73e9f996a3
Macfe1 <mac1515.pp@gmail.com>
2024-04-25 10:04:06 -0500

    Agregando 2-mul.c el cual multiplica dos enteros

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Do the multiplication
+ *
+ * @a: multiplier
+ * @b: multiplicand
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
