85ab6f7a532775860bc5d1ec35cbace48050c8d3
Macfe1 <mac1515.pp@gmail.com>
2024-04-25 15:48:35 -0500

    Agregando 5-more_numbers.c en donde imprime los numeros del 0 al 14, tenía un error porque no imprimia los dos digitos los mayores a 9

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print the numbers from 0 to 14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int numeros;
+	int veces;
+
+	for (veces = 1; veces <= 10; veces++)
+
+	{
+		for (numeros = 0; numeros <= 14; numeros++)
+		{
+			if (numeros > 9)
+			{
+				_putchar('0' + numeros / 2);
+			}
+
+			_putchar('0' + numeros % 10);
+		}
+
+	_putchar ('\n');
+
+	}
+}
