fb6f7274d1c5b97a5bfd2784936024a7ab34dadd
Macfe1 <mac1515.pp@gmail.com>
2024-04-25 11:32:11 -0500

    Agregando 4 que imprime los numeros de 0 a 9 menos el 2 y el 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - To print numbers from 0 to 9 but not 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int numero;
+
+	for (numero = 0; numero <= 9; numero++)
+	{
+		if (numero != 2 && numero != 4)
+		{
+			_putchar(numero);
+		}
+
+		_putchar('\n');
+	}
+}
