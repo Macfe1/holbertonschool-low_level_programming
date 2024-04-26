220a65d7468ba50a1447255a2c23e2a556ee85f4
Macfe1 <mac1515.pp@gmail.com>
2024-04-25 20:57:24 -0500

    Agregando 8-print_square.c en donde se imprime un cuadrado de #, faltaba la condicion de 0 y menores a cero pero con un if lo hice

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,11 @@ void print_square(int size)
 	int fila;
 	int columna;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (fila = 0; fila < size; fila++)
 	{
 		for (columna = 0; columna < size; columna++)
