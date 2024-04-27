05a6db6c10e0c7cf3b0843c061a4139cc8e0e58d
Macfe1 <mac1515.pp@gmail.com>
2024-04-26 22:38:00 -0500

    Agregando 9 el error es que no habia descripcion y adicioanl no tenia salto de linea al llegar a 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@ int main(void)
 		if (index % 5 == 0 && index % 3 == 0)
 
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 
 		else if (index % 3 == 0)
 
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 
 		else if (index % 5 == 0)
 
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 
 		else
