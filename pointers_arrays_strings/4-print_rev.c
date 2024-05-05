01e39acd417738d2f084bb0a7a95b1da8403cf7f
Macfe1 <mac1515.pp@gmail.com>
2024-05-04 22:58:02 -0500

    Agregando 4-print_rev.c imprime un string alreves

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ *
+ * @s: pointer
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		s++;
+	}
+
+	s--;
+
+	while (*s >= 0)
+
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
