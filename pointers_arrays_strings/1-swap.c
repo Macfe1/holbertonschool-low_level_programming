14f175b387172e2d9123512f43c0afc225ff7207
Macfe1 <mac1515.pp@gmail.com>
2024-05-04 19:14:28 -0500

    Agregando 1-swap.c en dondese les intercambian los valores a los punteros

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
+ * swap_int - function to swap the values
  *
+ * @a: pointer
+ * @b: pointer
  *
- *
- *
+ * Return: void 
  */
 void swap_int(int *a, int *b)
 {
-
+	int letra_a = *a;
+	*a = *b;
+	*b = letra_a;
 }
