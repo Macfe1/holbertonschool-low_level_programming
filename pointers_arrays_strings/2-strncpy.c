059e55753513c42ebf3483cada5e969e77e78523
Macfe1 <mac1515.pp@gmail.com>
2024-05-08 19:18:38 -0500

    Agregando 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,21 +12,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int length = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		length++;
-	}
-
-	if (n < length)
-	{
-		length = n;
-	}
-
-	for (i = 0; i < length; i++)
+	for (i = 0; src[i] && i != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
