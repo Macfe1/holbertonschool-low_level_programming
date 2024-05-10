d4b8ab6933d891851df282d63bd67b7df37691ff
Macfe1 <mac1515.pp@gmail.com>
2024-05-10 00:07:24 -0500

    Adding 5-string_toupper.c where an array of lowercase letters change to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ *
+ * @p: pointer to an array
+ *
+ * Return: pointer to the uppercase array
+ */
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 'a' + 'A';
+		}
+	}
+	return (p);
+}
