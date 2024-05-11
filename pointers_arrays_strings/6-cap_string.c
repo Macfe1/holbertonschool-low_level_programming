2a5e4807a1cd6ca1582dc044040f07f14b3a4af3
Macfe1 <mac1515.pp@gmail.com>
2024-05-10 21:27:33 -0500

    Agregando 6-cap_string.c, falto el betty y hacer que el primer caracter tambien se capitalizara

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,11 +14,17 @@ char *cap_string(char *s)
 	int j = 0;
 	char cadena[] = ",;.!?\"(){}' '";
 
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+	}
+
 	for (; s[i] != '\0'; i++)
 	{
 		for (j = 0; cadena[j] != '\0'; j++)
 		{
-			if ((s[i] == cadena[j] || s[i] == ' ' || s[i] == '\n' || s[i] == '\t') && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			if ((s[i] == cadena[j] || s[i] == ' ' || s[i] == '\n' || s[i] == '\t') &&
+				(s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			{
 				s[i + 1] = s[i + 1] - 32;
 			}
