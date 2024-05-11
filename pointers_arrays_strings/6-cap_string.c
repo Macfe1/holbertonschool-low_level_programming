22113b5e7492ffaee11d80500cbeef0f20a345c4
Macfe1 <mac1515.pp@gmail.com>
2024-05-10 21:15:40 -0500

    Agregando 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,13 @@ char *cap_string(char *s)
 {
 	int i = 0;
 	int j = 0;
-	char cadena[] = ",;.!?\"(){}";
+	char cadena[] = ",;.!?\"(){}' '";
 
 	for (; s[i] != '\0'; i++)
 	{
-		for (; cadena[j] != '\0'; j++)
+		for (j = 0; cadena[j] != '\0'; j++)
 		{
-			if (s[i] == cadena[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			if ((s[i] == cadena[j] || s[i] == ' ' || s[i] == '\n' || s[i] == '\t') && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			{
 				s[i + 1] = s[i + 1] - 32;
 			}
