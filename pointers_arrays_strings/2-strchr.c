ac12de05fd6c2cfaffb585ab484caf17c01217b5
Macfe1 <mac1515.pp@gmail.com>
2024-05-16 07:50:41 -0500

    Ajustando 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,14 +14,14 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-
 		if (c == '\0')
 		{
 			return ('\0');
+		}
+
+		if (s[i] == c)
+		{
+			return (&s[i]);
 		}
 	}
 		return (NULL);
