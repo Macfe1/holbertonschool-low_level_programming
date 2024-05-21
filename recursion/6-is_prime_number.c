55194648b1ce11807531c23f1f733038443573a3
Macfe1 <mac1515.pp@gmail.com>
2024-05-21 15:21:52 -0500

    Adding 6-is_prime_number.c to know if the input is an integer or not

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * aux - function to know if n is a prime number
+ *
+ * @prime: index number
+ * @n:number
+ *
+ * Return: 1 if n is integer otherwise 0
+ */
+int aux (int prime, int n)
+{
+	if (n <= 1 || (prime != n && n % prime == 0))
+		return (0);
+
+	if (prime == n)
+		return(1);
+	return (aux(prime + 1, n));
+}
+
+/**
+ * is_prime_number - function to know if n is integer or not
+ *
+ * @n: number
+ *
+ * Return: aux function
+ */
+int is_prime_number(int n)
+{
+	return (aux(2,n));
+}
