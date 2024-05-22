6e84f42be2c6e7047b5145db95e725941ddfc44a
Macfe1 <mac1515.pp@gmail.com>
2024-05-21 19:29:38 -0500

    Adding file

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * main - function to print their own name
+ *
+ * @argc: unused
+ * @argv: array
+ *
+ * Return: 0
+ */
+
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", *argv);
