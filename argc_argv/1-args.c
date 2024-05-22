b5506b643021391d78502c2880ec7925ea75bb13
Macfe1 <mac1515.pp@gmail.com>
2024-05-21 19:46:31 -0500

    Adding 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function to print the number of arguments
+ *
+ * @argc: number of arguments
+ * @argv: unused
+ *
+ * Return: argc
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
