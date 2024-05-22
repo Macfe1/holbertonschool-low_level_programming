d971892e13c1e65a27e24a64a7e95a22b633d83a
Macfe1 <mac1515.pp@gmail.com>
2024-05-21 20:04:21 -0500

    Adding 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints all arguments
+ *
+ * @argc: unused
+ * @argv: print all the arguments, one per line
+ *
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[i] != NULL)
+	{
+		printf("%s\n", *argv);
+		i += 1;
+	}
+	return (0);
+}
