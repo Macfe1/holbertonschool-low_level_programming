01a4ceb1af6f2fcf37bd350767f2a48b05763a59
Macfe1 <mac1515.pp@gmail.com>
2024-05-31 19:22:27 -0500

    Adding 1-string_nconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: Pointer
+ *
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int longitd = 0;
+
+	while (*s != '\0')
+	{
+		longitd++;
+		s++;
+	}
+
+	return (longitd);
+}
+
+/**
+ * string_nconcat - function that concatenates two arrays or n bytes
+ *
+ * @s1: first array
+ * @s2: second array
+ * @n: number of bytes from second array
+ *
+ * Return: pointer to array
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len_s1 = 0, len_s2 = 0, sum_all = 0;
+	unsigned int i = 0;
+	char *new_array = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
+	sum_all = len_s1 + (n > len_s2 ? len_s2 : n);
+
+	new_array = (char *) malloc(sum_all * sizeof(char));
+
+	if (new_array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len_s1; i++)
+	{
+		new_array[i] = s1[i];
+	}
+
+	for (i = 0; i < sum_all - len_s1; i++)
+	{
+		new_array[len_s1 + i] = s2[i];
+	}
+
+	return (new_array);
+}
