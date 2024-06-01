8bed3ce75d16ffbb8b83b1c1b44cccea32bcfe15
Macfe1 <mac1515.pp@gmail.com>
2024-05-31 19:38:05 -0500

    Adding 1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -45,7 +45,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len_s1 = _strlen(s1);
 	len_s2 = _strlen(s2);
-	sum_all = len_s1 + (n > len_s2 ? len_s2 : n);
+	sum_all = len_s1 + (n > len_s2 ? len_s2 : n) + 1;
 
 	new_array = (char *) malloc(sum_all * sizeof(char));
 
