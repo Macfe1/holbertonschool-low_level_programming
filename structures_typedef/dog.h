56bb96a636a12c69526672421d7f56185bde6312
Macfe1 <mac1515.pp@gmail.com>
2024-06-07 15:07:17 -0500

    Adding dog.h where it is defined a structures named dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this structure provide a dog info
+ * @name: The name of the dog
+ * @age: The age of teh dog
+ * @owner: The owner's name
+ */
+
+/*Definition of the struct dog*/
+struct dog
+{
+char *name; /*The name of the dog*/
+float age; /*The age of the dog*/
+char *owner; /*The dog's owner'*/
+};
+
+#endif
