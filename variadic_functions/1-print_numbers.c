1bb0f9457af03b8af977aa9b3f6850f89bb0fef1
Macfe1 <mac1515.pp@gmail.com>
2024-06-14 18:59:15 -0500

    Adding 1_print:numbers.c, function that prints the numbers with a separator, without space

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (separator != NULL)
 		{
-			printf("%d %s", num, separator);
+			printf("%d%s", num, separator);
 		}
 	}
 	printf("%d", num);
