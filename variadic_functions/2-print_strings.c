f248f668764d29fed22977f1db68f5329e5935b1
Macfe1 <mac1515.pp@gmail.com>
2024-06-15 21:10:56 -0500

    Adding 2-print_strings.c function that print strings followed by a separator and a new line

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - function to print strings followed by a separator
+ *
+ * @separator: separator after the string
+ * @n: number of parameters to show
+ *
+ * Return: the n strings with its separator
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+	unsigned int i = 0;
+	char *next_strings;
+
+	va_list print_string;
+
+	if (separator == NULL)
+		separator = "";
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(print_string, n);
+
+	next_strings = va_arg(print_string, char*);
+
+	for (i = 0; i < n - 1; i++, next_strings = va_arg(print_string, char*))
+	{
+		if (next_strings == NULL)
+		{
+			printf("(nil)%s", separator);
+		}
+
+		printf("%s%s", next_strings, separator);
+	}
+
+	if (next_strings == NULL)
+	{
+		printf("(nil)");
+	}
+
+	printf("%s", next_strings);
+
+	va_end(print_string);
+
+	printf("\n");
+}
