edf7d5c3dfb8b29074b699201ecaaae128ccb10b
Macfe1 <mac1515.pp@gmail.com>
2024-06-17 20:28:39 -0500

    Adding 3-print_all.c that prints all the arguments following the format, with the .h file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,5 +11,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/*structures*/
+
+typedef struct list_arg
+{
+	char name;
+	void (*print)(va_list arg);
+} list_arg;
+
+
 #endif
 
