d86858744663330b82e078e730ce8437fc9190d8
Macfe1 <mac1515.pp@gmail.com>
2024-07-06 21:47:06 -0500

    deleting

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -14,8 +14,8 @@ void free_list(list_t *head)
 	while (current_node != NULL)
 	{
 		next = current_node->next;
-		free(current_node);
 		free(current_node->str);
+		free(current_node);
 		current_node = next;
 	}
 }
