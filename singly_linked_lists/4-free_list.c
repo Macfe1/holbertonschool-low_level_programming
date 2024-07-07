e26dada2dbcc7536f0901a55a2db4a82b1f8b136
Macfe1 <mac1515.pp@gmail.com>
2024-07-06 21:29:57 -0500

    Adding in the lists.h the prototype of 4-free_list.c and also adding that file that frees the memory of the nodes from the linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees all the node memory
+ *
+ * @head: a pointer to the first node of the linked list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node = head, *next;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (current_node->next != NULL)
+	{
+		next = current_node->next;
+		free(current_node);
+		current_node = next;
+	}
+}
