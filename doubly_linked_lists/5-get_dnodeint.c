66da5a3654cce7d2ebe597ef48a8459da4bc1d18
Macfe1 <mac1515.pp@gmail.com>
2024-07-09 21:59:15 -0500

    Adding 5-get_dnodeint.c function that get the node in the index position

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * *get_dnodeint_at_index - get the nth node of a dlistint_t linked list.
+ *
+ * @head: pointer ti the first node
+ * @index: position to get the needed element
+ *
+ * Return: the node in the index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int counter = 0;
+
+	while (counter < index)
+	{
+		temp = temp->next;
+		counter++;
+	}
+
+	return (temp);
+}
