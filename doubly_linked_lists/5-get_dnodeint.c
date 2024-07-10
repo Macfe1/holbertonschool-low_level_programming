ed6fb31dce99fb8da043e332cde956386eb4101d
Macfe1 <mac1515.pp@gmail.com>
2024-07-09 22:04:21 -0500

    Adding special cases

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,11 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = head;
 	unsigned int counter = 0;
+	
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 
 	while (counter < index)
 	{
