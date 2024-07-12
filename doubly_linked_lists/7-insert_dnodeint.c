78015b484457c3b279413e5f23dbc4c55e03de1e
Macfe1 <mac1515.pp@gmail.com>
2024-07-11 23:48:50 -0500

    Adding the function  that inserts a new node at a given position and its prototype in the lists.h file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,13 +1,17 @@
-#inlcude "lists.h"
+#include "lists.h"
 
 /**
+ * *insert_dnodeint_at_index - inserts a new node at a given position
  *
+ * @h: pointer to teh first node
+ * @idx: info to know where paste the new node
+ * @n: data
  *
- * 
+ * Return: the addres of the new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *temp_head = *h;
+	dlistint_t *newnode, *temp_head = *h, *temp_nextnode;
 	unsigned int iterator = 0;
 
 	newnode = (dlistint_t *) malloc(sizeof(dlistint_t));
@@ -18,25 +22,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (temp_head == NULL)
 		return (NULL);
 
-	while (temp_head != NULL && iterator < idx)
+	if (idx == 0)
+	{
+		newnode->prev = NULL;
+		newnode->next = temp_head;
+
+		if (*h != NULL)
+			temp_head->prev = newnode;
+
+		*h = newnode;
+	}
+
+	while (temp_head != NULL && iterator < idx - 1)
 	{
 		if (temp_head->next == NULL)
 			return (NULL);
 		temp_head = temp_head->next;
 		iterator++;
 	}
-	/*temp_head = temp_head->next;*/
-	
+	temp_nextnode = temp_head->next;
 	newnode->n = n;
 	newnode->prev = temp_head;
-	newnode->next = temp_head->next
+	newnode->next = temp_head->next;
 
-	if (temp_head->next != NULL)
-		temp_head->next = newnode;
-	
-	
+	if (temp_nextnode != NULL)
+		temp_nextnode->prev = newnode;
 
+	temp_head->next = newnode;
 
-		
-
+	return (newnode);
 }
