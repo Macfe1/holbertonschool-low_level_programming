2f6b7ff9b1028705c1dab57ea8634d1cbd9469d0
Macfe1 <mac1515.pp@gmail.com>
2024-07-12 00:09:38 -0500

    Adding the case that head is NULL

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,8 +20,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (temp_head == NULL)
-		return (NULL);
-
+	{
+		newnode->n = n;
+		newnode->prev = NULL;
+		newnode->next = NULL;
+	}
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
