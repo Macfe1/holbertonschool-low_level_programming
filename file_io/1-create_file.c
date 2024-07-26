1cd8128520408f7746fe43c34cdd07f3c914e8e7
Macfe1 <mac1515.pp@gmail.com>
2024-07-25 21:30:53 -0500

    Adding the real and not the prove

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,63 +1,49 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include "main.h"
 
 /**
- * create_file - Create a file and write to it
+ * create_file - create a file and write it
  *
- * @filename: The name of the file to create
- * @text_content: The content to write to the file
+ * @filename: Is a NULL terminated string to write to the file
+ * @text_content: The name of the file to create
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if success otherwise -1
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor;
-    ssize_t bytes_written;
+	int file_descriptor = 0;
+	ssize_t bytes_written = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    // Check if the file exists
-    if (access(filename, F_OK) == 0)
-    {
-        // File exists, open it with write permissions and truncate it
-        file_descriptor = open(filename, O_WRONLY | O_TRUNC);
-        if (file_descriptor == -1)
-        {
-            perror("Failed to open file");
-            return (-1);
-        }
-    }
-    else
-    {
-        // File does not exist, create it with the specified permissions
-        file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-        if (file_descriptor == -1)
-        {
-            perror("Failed to create file");
-            return (-1);
-        }
-    }
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (text_content != NULL)
-    {
-        bytes_written = write(file_descriptor, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            perror("Failed to write to file");
-            close(file_descriptor);
-            return (-1);
-        }
-    }
+	if (file_descriptor == -1)
+	{
+		perror("fails");
+		return (-1);
+	}
 
-    if (close(file_descriptor) == -1)
-    {
-        perror("Failed to close file");
-        return (-1);
-    }
+	if (text_content == NULL)
+	{
+	}
 
-    return (1);
+	if (text_content != NULL)
+	{
+		bytes_written = write(file_descriptor, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			perror("fails");
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	if (close(file_descriptor))
+	{
+		perror("fails close");
+		return (0);
+	}
+	return (1);
 }
