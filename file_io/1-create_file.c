7165bbca966de126f45c318128f82dc526703dd9
Macfe1 <mac1515.pp@gmail.com>
2024-07-25 21:11:29 -0500

    Adding with betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY |O_RDONLY | O_TRUNC, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 	{
