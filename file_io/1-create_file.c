1601821fca36d54f3a91fad01f36ac1a12d87fe3
Macfe1 <mac1515.pp@gmail.com>
2024-07-25 20:33:10 -0500

    Adding in main.h the prototype of the 1-create_file.c and also adding that file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
+ * create_file - create a file and write it
  *
+ * @filename: Is a NULL terminated string to write to the file
+ * @text_content: The name of the file to create
  *
+ * Return: 1 if success otherwise -1
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor = 0;
-	ssize_t bytes_written = 0; 
+	ssize_t bytes_written = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open (filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file_descriptor == -1)
 	{
 		perror("fails");
@@ -27,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		bytes_written = write(file_descriptor, text_content, strlen(text_content));
-		
+
 		if (bytes_written == -1)
 		{
 			perror("fails");
@@ -36,6 +40,10 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	close(file_descriptor);
+	if (close(file_descriptor))
+	{
+		perror("fails close");
+		return (0);
+	}
 	return (1);
 }
