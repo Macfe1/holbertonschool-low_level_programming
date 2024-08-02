6188d82456179a525907e7540d62f90762597720
Macfe1 <mac1515.pp@gmail.com>
2024-08-02 11:52:09 -0500

    Adding in the hash_tables.h the prototype of 0-hash_table.c and its function

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_create - To create a hash table
+ *
+ * @size: Size of the table
+ *
+ * Return: if success return new table otherwise NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *new_table;
+
+	new_table = (hash_table_t *) malloc(sizeof(hash_table_t) * size);
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
+
+	return (new_table);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,7 +2,8 @@
 #define _HASH_TABLES_H_
 
 /*Libraries*/
-
+#include <stdlib.h>
+#include <stddef.h>
 /*Structures*/
 
 /**
@@ -15,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,8 +31,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /*Prototypes*/
