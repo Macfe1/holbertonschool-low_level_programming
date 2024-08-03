4c25969da8399b6547646836051aa8881ab573cc
Macfe1 <mac1515.pp@gmail.com>
2024-08-03 13:17:18 -0500

    Adding 3-hash_table_set.c and its prototype in the hash_tables.h

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ *
+ * @ht: pointer to the table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if success otherwise 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	char *dup_value = strdup(value), *dup_key = strdup(key);
+	hash_node_t *new_node, *current_node, *pointer_to_old_head;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	if (dup_value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	pointer_to_old_head = ht->array[index];
+	current_node = pointer_to_old_head;
+
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = dup_value;
+			return (1);
+		}
+
+		current_node = current_node->next;
+	}
+
+	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = dup_key;
+	new_node->next = pointer_to_old_head;
+	ht->array[index] = new_node;
+
+	return (1);
+}
