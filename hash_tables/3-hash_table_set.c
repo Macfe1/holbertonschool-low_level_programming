e285f493d9ab4944dffc75f39e3c9679e09e03bf
Macfe1 <mac1515.pp@gmail.com>
2024-08-04 22:29:41 -0500

    Adding 3-hash_table_set.c setting the pointers and the dinamic memorie

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,31 +19,36 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (dup_value == NULL)
 		return (0);
+	if (dup_key == NULL)
+	{
+		free(dup_value);
+		return (0);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 
 	pointer_to_old_head = ht->array[index];
 	current_node = pointer_to_old_head;
-
 	while (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
 		{
 			free(current_node->value);
 			current_node->value = dup_value;
+			free(dup_key);
 			return (1);
 		}
-
 		current_node = current_node->next;
 	}
-
 	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(dup_value);
+		free(dup_key);
 		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = dup_key;
+	}
+	new_node->key = dup_key;
+	new_node->value = dup_value;
 	new_node->next = pointer_to_old_head;
 	ht->array[index] = new_node;
-
 	return (1);
 }
